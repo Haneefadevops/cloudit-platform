efe22491ef15ab1b5ee9ab2c9c4d19f2621eb2be
CloudIT DevOps <devops@cloudit.local>
2026-07-03 08:17:45 +0200

    fix(platform-api): provide jwt service to modules guard

--- apps/platform-api/src/modules/modules.module.ts
+++ apps/platform-api/src/modules/modules.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { ModulesController } from './modules.controller';
 import { ModulesService } from './modules.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule.register({})],
   controllers: [ModulesController],
   providers: [ModulesService],
   exports: [ModulesService],
